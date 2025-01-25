ff5efe30dc500cae9f3f32a2187e0cd0b2bfd2f7
Ryan Purnama <ryan.purnama29@gmail.com>
2025-01-25 19:07:44 +0700

    Convert date to UTC format in normalizer function

--- src/app/api/scrap/normalizer.ts
+++ src/app/api/scrap/normalizer.ts
@@ -1,11 +1,11 @@
 import { type DataType, type Transaction } from './types';
 
 function convertStringToNumber(input: string): number {
-  if (input.startsWith("(") && input.endsWith(")")) {
+  if (input.startsWith('(') && input.endsWith(')')) {
     const numberString = input.slice(1, -1);
     return -parseFloat(numberString);
   }
-  if (input === "0.0" || input === "-") return 0;
+  if (input === '0.0' || input === '-') return 0;
   return parseFloat(input);
 }
 
@@ -16,11 +16,25 @@ export function normalizer(data: DataType): Transaction[] {
   data.body.forEach((bodyRow: string[]) => {
     const [date, ...values] = bodyRow;
     values.forEach((value, index) => {
+      // Konversi tanggal ke UTC
+      const localDate = new Date(date); // Tanggal dari input (local time)
+      const utcDate = new Date(
+        Date.UTC(
+          localDate.getFullYear(),
+          localDate.getMonth(),
+          localDate.getDate(),
+          localDate.getHours(),
+          localDate.getMinutes(),
+          localDate.getSeconds(),
+          localDate.getMilliseconds(),
+        ),
+      );
+
       result.push({
         companyName: companyNames[index],
         etfSymbol: etfNames[index],
         date,
-        formatedDate: new Date(date), //UTC format
+        formatedDate: utcDate,
         amount: convertStringToNumber(`${value}`),
       });
     });
