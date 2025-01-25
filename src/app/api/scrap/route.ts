8e74ce1071482444e97257df508854935c57cb3c
Ryan Purnama <ryan.purnama29@gmail.com>
2025-01-25 13:16:53 +0700

    coba

--- src/app/api/scrap/route.ts
+++ src/app/api/scrap/route.ts
@@ -1,57 +1,57 @@
-import { NextResponse } from 'next/server'
-import prisma from '@/lib/prisma/prisma';
-import { scrapeETFData } from "./scrape";
-import { normalizer } from "./normalizer";
-import { type Transaction } from "./types";
+import { NextResponse } from 'next/server';
+// import prisma from '@/lib/prisma/prisma';
+import { scrapeETFData } from './scrape';
+// import { normalizer } from "./normalizer";
+// import { type Transaction } from "./types";
 // import { MOCK_DATA } from "./mock";
 
 // async function deleteAllTransactions() {
 //   return prisma.transaction.deleteMany();
 // }
 
-async function insertTransactions(normalizeData: Transaction[]) {
-  try {
-    const upsertPromises = normalizeData.map(async (transaction) => {
-      // Cari ETF berdasarkan etf_name dan company_name
-      const etf = await prisma.etf.findFirst({
-        where: {
-          etf_symbol: transaction.etfSymbol,
-          company_name: transaction.companyName,
-        },
-      });
+// async function insertTransactions(normalizeData: Transaction[]) {
+//   try {
+//     const upsertPromises = normalizeData.map(async (transaction) => {
+//       // Cari ETF berdasarkan etf_name dan company_name
+//       const etf = await prisma.etf.findFirst({
+//         where: {
+//           etf_symbol: transaction.etfSymbol,
+//           company_name: transaction.companyName,
+//         },
+//       });
 
-      if (!etf) {
-        console.error(`ETF not found for ${transaction.etfSymbol} (${transaction.companyName})`);
-        return;
-      }
+//       if (!etf) {
+//         console.error(`ETF not found for ${transaction.etfSymbol} (${transaction.companyName})`);
+//         return;
+//       }
 
-      // Gunakan upsert untuk membuat atau memperbarui transaksi
-      await prisma.transaction.upsert({
-        where: {
-          etf_id_date: { // Gunakan constraint unik untuk etf_id dan date
-            etf_id: etf.id,
-            date: new Date(transaction.formatedDate),
-          },
-        },
-        update: {
-          amount: transaction.amount, // Perbarui amount jika data sudah ada
-        },
-        create: {
-          etf_id: etf.id,
-          amount: transaction.amount,
-          date: new Date(transaction.formatedDate), // Buat data baru jika tidak ada
-        },
-      });
+//       // Gunakan upsert untuk membuat atau memperbarui transaksi
+//       await prisma.transaction.upsert({
+//         where: {
+//           etf_id_date: { // Gunakan constraint unik untuk etf_id dan date
+//             etf_id: etf.id,
+//             date: new Date(transaction.formatedDate),
+//           },
+//         },
+//         update: {
+//           amount: transaction.amount, // Perbarui amount jika data sudah ada
+//         },
+//         create: {
+//           etf_id: etf.id,
+//           amount: transaction.amount,
+//           date: new Date(transaction.formatedDate), // Buat data baru jika tidak ada
+//         },
+//       });
 
-      // console.log(`Upserted transaction for ETF: ${transaction.etfSymbol} on ${transaction.formatedDate.toLocaleString('id-ID')}`);
-    });
+//       // console.log(`Upserted transaction for ETF: ${transaction.etfSymbol} on ${transaction.formatedDate.toLocaleString('id-ID')}`);
+//     });
 
-    // Jalankan semua upsert secara paralel
-    await Promise.all(upsertPromises);
-  } catch (error) {
-    console.error('Error inserting transactions:', error);
-  }
-}
+//     // Jalankan semua upsert secara paralel
+//     await Promise.all(upsertPromises);
+//   } catch (error) {
+//     console.error('Error inserting transactions:', error);
+//   }
+// }
 
 export async function GET() {
   const scrappedData = await scrapeETFData();
@@ -62,10 +62,10 @@ export async function GET() {
     header: scrappedData.header,
     body: scrappedData.body.slice(-3), // Ambil 3 data terakhir
   };
-  
-  const normalizeData = normalizer(get3LastData);
 
-  await insertTransactions(normalizeData);
+  // const normalizeData = normalizer(get3LastData);
+
+  // await insertTransactions(normalizeData);
 
   // const allTransactions = await getAllTransactions();
   return NextResponse.json({ data: get3LastData }, { status: 200 });
