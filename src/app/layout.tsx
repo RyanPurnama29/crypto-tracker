8eb82f965e45bf0e72348de29f7c323f3d1e4633
Ryan Purnama <ryan.purnama29@gmail.com>
2025-01-25 19:00:37 +0700

    add speed insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,21 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "Crypto Tracker",
-  description: "Crypto Tracker",
+  title: 'Crypto Tracker',
+  description: 'Crypto Tracker',
 };
 
 export default function RootLayout({
@@ -28,6 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg:white dark:bg-slate-900 text-black dark:text-white`}
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
